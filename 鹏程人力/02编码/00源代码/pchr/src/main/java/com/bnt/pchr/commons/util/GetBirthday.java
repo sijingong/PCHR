df013f4167d41bfad3abe2f15f9449a138d3b2a6
wuwenjie <wuwenjie5495@vip.qq.com>
2022-07-15 22:04:13 +0800

    7月15生日提醒功能

---
+++ 鹏程人力/02编码/00源代码/pchr/src/main/java/com/bnt/pchr/commons/util/GetBirthday.java
@@ -0,0 +1,37 @@
+package com.bnt.pchr.commons.util;
+
+import com.bnt.pchr.entity.Emp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 计算生日
+ */
+public  class GetBirthday {
+    public  String bir(Emp emp){
+        //得到当前年
+        String fa="啥也不干";
+        SimpleDateFormat sfm=new SimpleDateFormat("yyyy");
+        int nowYear=  Integer.valueOf(sfm.format(new Date()));
+        int nowYearDay=0;
+        if (nowYear/400==0){
+            //今年是闰年
+            nowYearDay=366;
+        }else {
+            nowYearDay=365;
+        }
+        //得到年中天
+        Calendar ca=Calendar.getInstance();
+        ca.setTime(new Date());
+        int  nowDayInYear=ca.get(Calendar.DAY_OF_YEAR);
+        //得到员工出生年份
+        int empYear=Integer.valueOf(sfm.format(emp.getBirthday()));
+        ca.setTime(emp.getBirthday());
+        int empDayInYear=ca.get(Calendar.DAY_OF_YEAR);
+        if (nowDayInYear-empDayInYear==364||empDayInYear-nowDayInYear==1){
+            fa="发送公告";
+        }
+        return fa;
+    }
+}
