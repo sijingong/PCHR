4810389220a8d59c2b39f9c6c587c28262ad5d47
wuwenjie <wuwenjie5495@vip.qq.com>
2022-07-16 17:38:29 +0800

    7月16日完善生日提醒功能思路

--- 鹏程人力/02编码/00源代码/pchr/src/main/java/com/bnt/pchr/commons/util/GetBirthday.java
+++ 鹏程人力/02编码/00源代码/pchr/src/main/java/com/bnt/pchr/commons/util/GetBirthday.java
@@ -1,6 +1,7 @@
 package com.bnt.pchr.commons.util;
 
 import com.bnt.pchr.entity.Emp;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -8,30 +9,31 @@ import java.util.Date;
 /**
  * 计算生日
  */
-public  class GetBirthday {
-    public  String bir(Emp emp){
+public class GetBirthday {
+    public static String bir(Emp emp) {
         //得到当前年
-        String fa="啥也不干";
-        SimpleDateFormat sfm=new SimpleDateFormat("yyyy");
-        int nowYear=  Integer.valueOf(sfm.format(new Date()));
-        int nowYearDay=0;
-        if (nowYear/400==0){
+        String notes = null;
+        SimpleDateFormat sfm = new SimpleDateFormat("yyyy");
+        int nowYear = Integer.valueOf(sfm.format(new Date()));
+        int nowYearDay = 0;
+        if (nowYear / 400 == 0) {
             //今年是闰年
-            nowYearDay=366;
-        }else {
-            nowYearDay=365;
+            nowYearDay = 366;
+        } else {
+            nowYearDay = 365;
         }
         //得到年中天
-        Calendar ca=Calendar.getInstance();
+        Calendar ca = Calendar.getInstance();
         ca.setTime(new Date());
-        int  nowDayInYear=ca.get(Calendar.DAY_OF_YEAR);
+        int nowDayInYear = ca.get(Calendar.DAY_OF_YEAR);
         //得到员工出生年份
-        int empYear=Integer.valueOf(sfm.format(emp.getBirthday()));
+        int empYear = Integer.valueOf(sfm.format(emp.getBirthday()));
         ca.setTime(emp.getBirthday());
-        int empDayInYear=ca.get(Calendar.DAY_OF_YEAR);
-        if (nowDayInYear-empDayInYear==364||empDayInYear-nowDayInYear==1){
-            fa="发送公告";
+        int empDayInYear = ca.get(Calendar.DAY_OF_YEAR);
+        if (nowDayInYear - empDayInYear == 364 || empDayInYear - nowDayInYear == 1) {
+
+            notes = emp.getEmpName() + "明天生日";
         }
-        return fa;
+        return notes;
     }
 }
